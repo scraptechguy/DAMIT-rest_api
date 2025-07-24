a3c6abecb1d04dff0b6f306d381f06892286df6a
Rostislav Brož <scraptechguy@gmail.com>
2025-07-24 19:33:37 +0200

    return something friendly at /

--- app/main.py
+++ app/main.py
@@ -16,3 +16,7 @@ def get_db():
 @app.get("/asteroids/", response_model=list[schemas.AsteroidRead])
 def read_asteroids(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_asteroids(db, skip=skip, limit=limit)
+
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI DAMIT API is running!"}
